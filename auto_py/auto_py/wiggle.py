194c9461161633110b82837a84283f3d81096540
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-07-24 12:36:07 -0400

    Wiggle (#1)
    * First test of wiggle maneuver
    
    * Add wiggle as a rounded rectangle
    
    ---------
    
    Co-authored-by: spencer@primus <spencer.hallyburton@duke.edu>
    Co-authored-by: cpsl-jetson-1 <cpsl-jetson-1>

--- auto_py/auto_py/wiggle.py
+++ auto_py/auto_py/wiggle.py
@@ -1,16 +1,58 @@
+#wiggle code below
 import rclpy
-
-from .base import AutoControl, AutoControlException
+import ackermann_msgs.msg
+import time
+from .base import set_member, AutoControl, AutoControlException
 
 
 class WiggleControl(AutoControl):
-    """
-    Basic control algorithm to make the f1tenth wiggle around
-    """
+    #drives in square (theoretcally)
 
-    def __init__(self):
+    def __init__(self, steering_angle: float = 0.0, counter: int = 0,start_time: float = time.time()):
         super().__init__()
+        self.steering_angle = steering_angle
+        self.counter = counter
+        self.start_time = start_time
 
+    def get_control_command(self) -> ackermann_msgs.msg.AckermannDriveStamped:
+        # get ackermann command
+        drive = ackermann_msgs.msg.AckermannDrive()
+        # -- lateral
+        self.steering_angle = get_steering_angle(self.counter)
+        set_member(drive, "steering_angle", self.steering_angle)
+     
+        set_member(drive, "steering_angle_velocity", self.steering_angle)
+        # -- longitudinal
+        set_member(drive, "speed", 1.0)
+        set_member(drive, "acceleration", 0.0)
+        set_member(drive, "jerk", 0.0)
+
+        # package in stamped message
+        msg = ackermann_msgs.msg.AckermannDriveStamped()
+        set_member(msg.header, "stamp", self.get_clock().now().to_msg())
+        set_member(msg, "drive", drive)
+
+        #time & counter tracking
+        if self.counter == 0:
+            if time.time() >= self.start_time + 1.0:
+                self.counter = self.counter + 1
+                self.start_time = time.time()
+        if self.counter == 1:
+            if time.time() >= self.start_time +1.33:
+                self.counter = self.counter - 1
+                self.start_time = time.time()
+        
+        return msg
+
+        
+def get_steering_angle(count):
+    ang = 0.0
+    if count == 0:
+        ang = 0.0
+    elif count == 1:
+        ang = 0.78539
+    return ang
+ 
 
 def main(args=None):
     rclpy.init(args=args)
@@ -25,3 +67,4 @@ def main(args=None):
 
     node.destroy_node()
     rclpy.shutdown()
+
