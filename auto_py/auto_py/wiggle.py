971dad7d3d08b524dbc9b7bbc8c7b4d6bc383f64
cpsl-jetson-1 <cpsl-jetson-1>
2024-07-24 12:33:33 -0400

    Add wiggle as a rounded rectangle

--- auto_py/auto_py/wiggle.py
+++ auto_py/auto_py/wiggle.py
@@ -1,32 +1,29 @@
+#wiggle code below
 import rclpy
 import ackermann_msgs.msg
 import time
-
 from .base import set_member, AutoControl, AutoControlException
 
 
 class WiggleControl(AutoControl):
-    """
-    Basic control algorithm to make the f1tenth wiggle around
-    """
+    #drives in square (theoretcally)
 
-    def __init__(self, wiggle_time: float = 1.0, angle: float = 0.524):
+    def __init__(self, steering_angle: float = 0.0, counter: int = 0,start_time: float = time.time()):
         super().__init__()
-
-        self.wiggle_time = wiggle_time
-        self.t_maneuver_start = None
-        self.polarity = 1
-        self.angle = angle
+        self.steering_angle = steering_angle
+        self.counter = counter
+        self.start_time = start_time
 
     def get_control_command(self) -> ackermann_msgs.msg.AckermannDriveStamped:
         # get ackermann command
         drive = ackermann_msgs.msg.AckermannDrive()
         # -- lateral
-        ang = self.set_steering_angle()
-        set_member(drive, "steering_angle", ang)
-        set_member(drive, "steering_angle_velocity", 0.5)
+        self.steering_angle = get_steering_angle(self.counter)
+        set_member(drive, "steering_angle", self.steering_angle)
+     
+        set_member(drive, "steering_angle_velocity", self.steering_angle)
         # -- longitudinal
-        set_member(drive, "speed", 0.5)
+        set_member(drive, "speed", 1.0)
         set_member(drive, "acceleration", 0.0)
         set_member(drive, "jerk", 0.0)
 
@@ -35,21 +32,26 @@ class WiggleControl(AutoControl):
         set_member(msg.header, "stamp", self.get_clock().now().to_msg())
         set_member(msg, "drive", drive)
 
+        #time & counter tracking
+        if self.counter == 0:
+            if time.time() >= self.start_time + 1.0:
+                self.counter = self.counter + 1
+                self.start_time = time.time()
+        if self.counter == 1:
+            if time.time() >= self.start_time +1.33:
+                self.counter = self.counter - 1
+                self.start_time = time.time()
+        
         return msg
 
-    def set_steering_angle(self) -> None:
-        # increment the time in this maneuver
-        if self.t_maneuver_start is None:
-            self.t_maneuver_start = 1e-9 * self.get_clock().now().nanoseconds
-        t_now = 1e-9 * self.get_clock().now().nanoseconds
-
-        # determine when it's time to switch the mode
-        if (t_now - self.t_maneuver_start) > self.wiggle_time:
-            self.polarity *= -1
-            self.t_maneuver_start = t_now
-
-        # return the desired angle
-        return self.polarity * self.angle
+        
+def get_steering_angle(count):
+    ang = 0.0
+    if count == 0:
+        ang = 0.0
+    elif count == 1:
+        ang = 0.78539
+    return ang
  
 
 def main(args=None):
